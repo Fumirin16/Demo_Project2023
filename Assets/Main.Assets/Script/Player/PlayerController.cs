27c6bcbe85ed8e6f30f11110316f28d74b833bb8
Fumirin16 <90897959+Fumirin16@users.noreply.github.com>
2023-12-22 16:22:05 +0900

    カメラ切り替え実装

--- Assets/Main.Assets/Script/Player/PlayerController.cs
+++ Assets/Main.Assets/Script/Player/PlayerController.cs
@@ -8,48 +8,71 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // 左右回転の数値取得する変数
-    float _rot = 0.0f;
-    // 回転スピードを取得する変数
-    [Tooltip("回転スピード数字が大きいほど速くなる")]
-    //public float rotateSpeed = 0.5f;
-    private float rotateSpeed;
-    // 前後移動スピードを取得する変数
-    [Tooltip("前後スピード数字が大きいほど速くなる")]
-    //public float positionSpeed = 0.5f;
-    private float positionSpeed;
-    // 値を参照する変数
-    public ValueSettingManager settingManager;
-    public AudioManager audioManager;
-    Rigidbody _rb;
+    #region ---Fields---
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// 左右回転の数値を取得する変数
+    /// </summary>
+    float _rot;
+
+    /// <summary>
+    /// 回転スピードを取得する変数数字が大きいほど速くなる
+    /// </summary>
+    float _rotateSpeed;
+
+    /// <summary>
+    /// 前後移動スピードを取得する変数数字が大きいほど速くなる
+    /// </summary>
+    float _positionSpeed;
+
+    /// <summary>
+    /// カメラが切り替え判定
+    /// </summary>
+    bool _cameraActive;
+
+    /// <summary>
+    /// カメラオブジェクト参照する変数
+    /// </summary>
+    [SerializeField] GameObject _mainCamera;
+    [SerializeField] GameObject _subCamera;
+
+    /// <summary>
+    /// ValueSettingManager参照する変数
+    /// </summary>
+    public ValueSettingManager settingManager;
+
+    /// <summary>
+    /// AudioManager参照する変数
+    /// </summary>
+    public AudioManager audioManager;
+
+    #endregion ---Fields---
+
+    #region ---Methods---
+
     void Start()
     {
-        // 値を管理するアセットから値を参照して変数に保存する
-        rotateSpeed = settingManager.PlayerRotateSpeed;
-        positionSpeed = settingManager.JOYSTIC_PlayerMoveSpeed;
-        _rb = gameObject.GetComponent<Rigidbody>();
+        // 値を参照したものを保存する
+        _rotateSpeed = settingManager.PlayerRotateSpeed;
+        _positionSpeed = settingManager.JOYSTIC_PlayerMoveSpeed;
+
+        //サブカメラを非アクティブにする
+        _subCamera.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        _rb.AddForce(new Vector3(0, 0, 0));
-        _rb.velocity = Vector3.zero;
         // 左右回転の数値取得
         _rot = Input.GetAxis("Horizontal");
-        //Debug.Log(_rot);
 
         // 回転
-        transform.Rotate(new Vector3(0, _rot * -rotateSpeed, 0));
+        transform.Rotate(new Vector3(0, _rot * -_rotateSpeed, 0));
 
         // 前後移動
         // 前
         if (Input.GetKey(KeyCode.JoystickButton1) || Input.GetKey(KeyCode.W))
         {
-            //_rb.AddForce(transform.forward * positionSpeed);
-            transform.position += transform.forward * positionSpeed;
+            transform.position += transform.forward * _positionSpeed;
 
             if (audioManager.CheckPlaySound(audioManager.seAudioSource))
             {
@@ -59,8 +82,7 @@ public class PlayerController : MonoBehaviour
         // 後ろ
         if (Input.GetKey(KeyCode.JoystickButton2) || Input.GetKey(KeyCode.S))
         {
-            //_rb.AddForce(-transform.forward * positionSpeed);
-            transform.position -= transform.forward * positionSpeed;
+            transform.position -= transform.forward * _positionSpeed;
 
             if (audioManager.CheckPlaySound(audioManager.seAudioSource))
             {
@@ -70,10 +92,28 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.JoystickButton1) || Input.GetKeyUp(KeyCode.JoystickButton2))
         {
-           if (!audioManager.CheckPlaySound(audioManager.seAudioSource))
+            if (!audioManager.CheckPlaySound(audioManager.seAudioSource))
             {
                 audioManager.StopSound(audioManager.seAudioSource);
             }
         }
+
+        // ジョイコンの右スティックを押すとメインカメラとサブカメラを切り替える
+        if (Input.GetKeyDown(KeyCode.JoystickButton11) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if(_cameraActive)
+            {
+                _mainCamera.SetActive(false);
+                _subCamera.SetActive(true);
+                _cameraActive = false;
+            }
+            else
+            {
+                _mainCamera.SetActive(true);
+                _subCamera.SetActive(false);
+                _cameraActive = true;
+            }
+        }
     }
+    #endregion ---Methods---
 }
