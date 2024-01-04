def8c6dd55df2b8f78976836d1e73eda4c59858e
Fumirin16 <90897959+Fumirin16@users.noreply.github.com>
2024-01-05 01:24:20 +0900

    文字化け

--- Assets/Main.Assets/Script/Guardsman/GuardsmanCollision.cs
+++ Assets/Main.Assets/Script/Guardsman/GuardsmanCollision.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-// 警備員のゲームオーバー判定
+// 警備員とじらじょちゃんの当たり判定
 // 作成者：地引翼
 
 public class GuardsmanCollision : MonoBehaviour
@@ -10,29 +8,25 @@ public class GuardsmanCollision : MonoBehaviour
     /// <summary>
     ///  値を管理するアセットから値を参照する
     /// </summary>
+    [Tooltip("ValueSettingManagerをアタッチ")]
     [SerializeField] ValueSettingManager _settingManager;
-
-    /// <summary>
-    ///  警備員オブジェクト取得
-    /// </summary>
-    [SerializeField] GameObject _guardsmanObj;
 
     /// <summary>
     ///  プレイヤーオブジェクト取得
     /// </summary>
+    [Tooltip("playerオブジェクトをアタッチ")]
     [SerializeField] GameObject _playerObj;
 
-    public void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
+        // タグ：Player
         if (other.gameObject.CompareTag("Player"))
         {
             // ゲームオーバーの判定をtrueにする
-            //_settingManager.gameOver = true;
+            _settingManager.gameOver = true;
 
             // プレイヤーのほうに向かせる
-            _guardsmanObj.transform.LookAt(_playerObj.transform.position);
-            
-            //Debug.Log("ゲームオーバー");
+            transform.parent.gameObject.transform.LookAt(_playerObj.transform.position);
         }
     }
 }
